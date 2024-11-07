0914eaf66e4503dc6a69839c5f80cd2d6a039051
IvanShaptalov <ivan.shaptalov1@gmail.com>
2024-11-07 19:26:45 +0100

    Initial Commit

---
+++ Snap Check Money Under Control/Snap_Check_Money_Under_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snap_Check_Money_Under_ControlApp.swift
+//  Snap Check Money Under Control
+//
+//  Created by PowerMac on 07.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Snap_Check_Money_Under_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
