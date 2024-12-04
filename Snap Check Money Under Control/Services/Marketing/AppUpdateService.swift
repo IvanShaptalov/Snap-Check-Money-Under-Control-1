b34ec56b50696f7cbaa0024fde51d51dee29f44d
IvanShaptalov <ivan.shaptalov1@gmail.com>
2024-12-04 14:53:22 +0100

    Fix app updater

--- Snap Check Money Under Control/Services/Marketing/AppUpdateService.swift
+++ Snap Check Money Under Control/Services/Marketing/AppUpdateService.swift
@@ -6,15 +6,18 @@ class UpdateChecker: ObservableObject {
     @Published var errorMessage: String?
     
     func checkForUpdate() async {
+        let timestamp = Int(Date().timeIntervalSince1970) // Текущая метка времени
         do {
             guard let info = Bundle.main.infoDictionary,
                   let currentVersion = info["CFBundleShortVersionString"] as? String,
                   let identifier = info["CFBundleIdentifier"] as? String,
-                  let url = URL(string: "https://itunes.apple.com/lookup?bundleId=\(identifier)") else {
+                  let url = URL(string: "https://itunes.apple.com/lookup?bundleId=\(identifier)&timestamp=\(timestamp)") else {
                 NSLog("🧐 version check > error while creating url")
                 isUpdateAvailable = false
                 return
             }
+            print("indent")
+            print(identifier)
             
             let (data, _) = try await URLSession.shared.data(from: url)
             let json = try JSONSerialization.jsonObject(with: data, options: [.allowFragments]) as? [String: Any]
